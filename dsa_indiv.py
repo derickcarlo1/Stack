439c6ed6403c7567f6c3639ec212b69ed609c89d
Derick Carlo S. Herrera <129716872+derickcarlo1@users.noreply.github.com>
2023-12-09 08:39:45 -0400

    mergelinkedlists.py

--- dsa_indiv.py
+++ dsa_indiv.py
@@ -3,7 +3,7 @@ class ListNode:
         self.value = value
         self.next = next
 
-def mergeTwoLists(list1, list2):
+def mergeTwoLists(list1, list2, value_limit):
     # dummy node as the head of the merged list
     dummy = ListNode(0)
     # pointer to traverse the merged list
@@ -11,6 +11,10 @@ def mergeTwoLists(list1, list2):
     
     # new nodes 
     while list1 and list2:
+        # Check if the node value exceeds the limit
+        if list1.value > value_limit or list2.value > value_limit:
+            raise ValueError("Node value exceeds the limit")
+
         if list1.value <= list2.value:
             current.next = ListNode(list1.value)
             list1 = list1.next
@@ -20,13 +24,25 @@ def mergeTwoLists(list1, list2):
         current = current.next
     
     # append remaining nodes from non-empty to merged list
-    if list1:
+    while list1:
+        if list1.value > value_limit:
+            raise ValueError("Node value exceeds the limit")
         current.next = list1
-    if list2:
+        list1 = list1.next
+        current = current.next
+    
+    while list2:
+        if list2.value > value_limit:
+            raise ValueError("Node value exceeds the limit")
         current.next = list2
+        list2 = list2.next
+        current = current.next
 
     # Return the head
     return dummy.next
+
+# Set the value limit
+value_limit = 3
 
 # proving the code from ex
 list1 = ListNode(1)
@@ -37,12 +53,14 @@ list2 = ListNode(1)
 list2.next = ListNode(3)
 list2.next.next = ListNode(4)
 
-# Merging 2 lists
-merged_list = mergeTwoLists(list1, list2)
-
-print("Merged List:")
-current_merged = merged_list
-while current_merged:
-    print(current_merged.value, end=" -> ")
-    current_merged = current_merged.next
-print("None")
+try:
+    # Merging 2 lists with value limit
+    merged_list = mergeTwoLists(list1, list2, value_limit)
+    print("Merged List:")
+    current_merged = merged_list
+    while current_merged:
+        print(current_merged.value, end=" -> ")
+        current_merged = current_merged.next
+    print("None")
+except ValueError as e:
+    print("Error:", e)
